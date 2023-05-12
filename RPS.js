83c45d50c809917be880ca7b3ed3ee7ec512d882
junilowahab <junilojoshuawahab@gmail.com>
2023-05-12 13:02:27 +0100

    changed the code for when a word is misspelt

--- RPS.js
+++ RPS.js
@@ -1,5 +1,5 @@
 //array containing the weapons
-const weapons = ['Rock', 'Paper', 'Scissors'];
+const weapons = ['rock', 'paper', 'scissors'];
 
 //function to select random weapon
 function game(){
@@ -19,34 +19,35 @@ function game(){
         userChoice.toLowerCase;
         console.log(userChoice);
 
-        //if user mispells a word
-        if((userChoice !== 'rock') || (userChoice !== 'paper') || (userChoice !== 'scissors')){
-            console.log('Misspelt word, try again');
-            i--;
-        }
-
-        //compare choices between user and computer if computer wins
-        if((computerChoice === 'Rock' && userChoice === 'scissors') || (computerChoice === 'Paper' && userChoice === 'rock') || (computerChoice === 'Scissors' && userChoice === 'paper')){
+        //if user mispells a word -go to line 47-
+        if((userChoice === 'rock') || (userChoice === 'paper') || (userChoice === 'scissors')){
+                
+            //compare choices between user and computer if computer wins
+            if((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') || (computerChoice === 'scissors' && userChoice === 'paper')){
             
-            //computer gains one point
-            computerScore++;
-            console.log(`Your computers score is ${computerScore}`);
-        };
+                //computer gains one point
+                computerScore++;
+                console.log(`Your computers score is ${computerScore}`);
+            };
 
-        //compare choices between user and computer if user wins
-        if((userChoice === 'rock' && computerChoice === 'Scissors') || (userChoice === 'paper' && computerChoice === 'Rock') || (userChoice === 'scissors' && computerChoice === 'Paper')){
+            //compare choices between user and computer if user wins
+            if((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') || (userChoice === 'scissors' && computerChoice === 'paper')){
 
-            //user gains one point
-            userScore++;
-            console.log(`Your score is ${userScore}`);
-        };
+                //user gains one point
+                userScore++;
+                console.log(`Your score is ${userScore}`);
+            };
 
-        //compare choices between user and computer if both chose same thing
-        if((userChoice === 'rock' && computerChoice === 'Rock') || (userChoice === 'paper' && computerChoice === 'Paper') || (userChoice === 'scissors' && computerChoice === 'Scissors')){
+            //compare choices between user and computer if both chose same thing
+            if((userChoice === 'rock' && computerChoice === 'rock') || (userChoice === 'paper' && computerChoice === 'paper') || (userChoice === 'scissors' && computerChoice === 'scissors')){
+                i--;
+                console.log('Tie! Try again');
+            };
+        }else{
+            console.log('Misspelt a word, try again');
             i--;
-            console.log('Tie! Try again');
-        }
+        };
     };
 };
 
-game()
+game();
